9516c961e3f81c285ab0ee4972eda1608fffabdf
Tejas <ttejas220@gmail.com>
2025-05-20 22:51:18 +0530

    fixed build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { File, mockFiles } from "../lib/mock-data";
+import { mockFiles } from "../lib/mock-data";
 import { Folder, FileIcon, Upload, ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
@@ -97,7 +97,7 @@ export default function GoogleDriveClone() {
                       </button>
                     ) : (
                       <Link
-                        href={file.url || "#"}
+                        href={file.url ?? "#"}
                         className="flex items-center text-gray-100 hover:text-blue-400"
                       >
                         <FileIcon className="mr-3" size={20} />
