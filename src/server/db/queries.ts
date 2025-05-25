4bf423d0b975d08dc2b7eac5286b0a5745ae7f45
Tejas <ttejas220@gmail.com>
2025-05-25 11:27:36 +0530

    fixed order

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -12,10 +12,15 @@ export const QUERIES = {
     return db
       .select()
       .from(folderSchema)
-      .where(eq(folderSchema.parent, folderId));
+      .where(eq(folderSchema.parent, folderId))
+      .orderBy(folderSchema.id);
   },
   getFiles: function (folderId: number) {
-    return db.select().from(fileSchema).where(eq(fileSchema.parent, folderId));
+    return db
+      .select()
+      .from(fileSchema)
+      .where(eq(fileSchema.parent, folderId))
+      .orderBy(fileSchema.id);
   },
   getAllParentsForFolders: async function (folderId: number) {
     const parents = [];
